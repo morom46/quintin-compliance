cbfabd758b5afbab9b7d10ea2229cb8d68a67e93
morom46 <arindombatsah46@gmail.com>
2025-05-04 20:54:19 +0530

    Committing latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/quintin-compliance/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
